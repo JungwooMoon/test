b2b9fdcc6d01914ca20694490954cb6b8133e55f
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-05-11 14:49:19 +0900

    init: 스프링 초기 설정

---
+++ src/test/java/serious/backend/doldol/DoldolApplicationTests.java
@@ -0,0 +1,13 @@
+package serious.backend.doldol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoldolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
